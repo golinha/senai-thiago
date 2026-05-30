37d7bed01b17eea339edff7074b4efd2ad75dfd6
Thiago Julio Ornelas Reis <ben0067162@aluno.firjansenaisesi.com.br>
2026-05-29 21:35:01 -0300

    terminar

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function myFunction() {
         alert("Hello World");
     }
+
+
+
+    function login() {
+        let usuario=document.getElementById("usuario");
+        let senha=document.getElementById("senha");
+    }
+
+
+        <function logout(){
+        let telaHome=document.getElementById("home").style.display="none";
+
+
+        let usuario=document.getElementById("usuario");
+        let senha=document.getElementById("senha");
